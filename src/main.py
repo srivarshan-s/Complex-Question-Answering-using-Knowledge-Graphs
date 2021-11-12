6ded0b1ef3c946eb61c4e744f743dc8f991c58bb
Srivarshan <srivarshan02@gmail.com>
2021-11-13 01:02:49 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,47 +1,35 @@
 # Importing Libraries
-
-from init import pd, nlp, tqdm, nx, plt
-from entity import get_entities, get_relation
-
-ROOT_PATH = '/run/media/sri/OS/Users/sriva/Desktop/Sem 5/'
-ROOT_PATH += 'Knowledge Representation/Project/'
-ROOT_PATH += 'Complex-Question-Answering-using-Knowledge-Graphs/'
+from init import pd, ROOT_PATH
+from knowledge_graph import load_data, plot_graph
+from knowledge_graph import extract_sub_obj, get_question, answer
 
 
 # Read the data
-
 candidate_sentences = pd.read_csv(ROOT_PATH + "data/wiki_sentences_v2.csv")
 candidate_sentences = candidate_sentences.head(200)
 
-entity_pairs = []
-relations = []
 
+# Load entity pairs and relations
 print("Loading Data...")
-for i in tqdm(candidate_sentences["sentence"]):
-  entity_pairs.append(get_entities(i))
-  relations.append(get_relation(i))
+entity_pairs, relations = load_data(
+        candidate_sentences["sentence"])
 
-# extract subject
-source = [i[0] for i in entity_pairs]
 
-# extract object
-target = [i[1] for i in entity_pairs]
+# Extract subject, object
+source, target = extract_sub_obj(entity_pairs)
 
+
+# Create knowledge graph dataframe
 kg_df = pd.DataFrame({'source':source, 'target':target, 'edge':relations})
 
-print("Enter your question:")
-question = input()
-subject, _ = get_entities(question)
-edge = get_relation(question)
 
-for i in range(kg_df.shape[0]):
-    if kg_df.source[i] == subject and kg_df.edge[i] == edge:
-        print(kg_df.source[i], edge, kg_df.target[i])
+# Get question from user
+subject, edge = get_question()
 
-# Plot Graph
-G = nx.from_pandas_edgelist(kg_df[kg_df['edge']==edge], "source", "target", 
-                          edge_attr=True, create_using=nx.MultiDiGraph())
-plt.figure(figsize=(12,12))
-pos = nx.spring_layout(G, k = 0.5) # k regulates the distance between nodes
-nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_cmap=plt.cm.Blues, pos = pos)
-plt.savefig(ROOT_PATH + "graph/" + edge + "_graph.png")
+
+# Obtain answer from knowledge graph
+answer(kg_df, subject, edge)
+
+
+# Plot knowledge graph
+plot_graph(kg_df, edge)
