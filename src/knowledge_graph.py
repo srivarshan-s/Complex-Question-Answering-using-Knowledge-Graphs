2db8c942bae115b70e22205311b9baaf7efe5ee3
Srivarshan <srivarshan02@gmail.com>
2021-11-13 01:03:37 +0530

    File name changed

--- src/knowledge_graph.py
+++ src/knowledge_graph.py
@@ -1,4 +1,51 @@
-from init import nlp, Matcher
+from init import nlp, Matcher, tqdm, plt, nx, ROOT_PATH
+
+
+def load_data(sentences):
+    
+    entity_pairs = []
+    relations = []
+
+    for i in tqdm(sentences):
+        entity_pairs.append(get_entities(i))
+        relations.append(get_relation(i))
+
+    return entity_pairs, relations
+
+
+def extract_sub_obj(entity_pairs):
+
+    subject = [ i[0] for i in entity_pairs ]
+    target = [ i[1] for i in entity_pairs ]
+
+    return subject, target
+
+
+def get_question():
+
+    print("Enter your question:")
+    question = input()
+    subject, _ = get_entities(question)
+    edge = get_relation(question)
+
+    return subject, edge
+
+
+def answer(dataframe, subject, edge):
+
+    for i in range(dataframe.shape[0]):
+        if dataframe.source[i] == subject and dataframe.edge[i] == edge:
+            print(dataframe.source[i], edge, dataframe.target[i])
+
+
+def plot_graph(dataframe, relation):
+
+    G = nx.from_pandas_edgelist(dataframe[dataframe['edge']==relation], "source", "target", 
+                              edge_attr=True, create_using=nx.MultiDiGraph())
+    plt.figure(figsize=(12,12))
+    pos = nx.spring_layout(G, k = 0.5) 
+    nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_cmap=plt.cm.Blues, pos = pos)
+    plt.savefig(ROOT_PATH + "graph/" + relation + "_graph.png")
 
 
 def get_entities(sent):
