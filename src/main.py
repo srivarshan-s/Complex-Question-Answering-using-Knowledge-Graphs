9cad2127834e881a07d4b9b835a67317a83fc665
Srivarshan <srivarshan02@gmail.com>
2021-11-13 00:22:56 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@
 from init import pd, nlp, tqdm, nx, plt
 from entity import get_entities, get_relation
 
-pd.set_option('display.max_colwidth', 200)
-
 ROOT_PATH = '/run/media/sri/OS/Users/sriva/Desktop/Sem 5/'
 ROOT_PATH += 'Knowledge Representation/Project/'
 ROOT_PATH += 'Complex-Question-Answering-using-Knowledge-Graphs/'
